22719ecfec0ff540bd8c12ef1697145367d92e6c
Ainurizam Ainuddin <rizainuddin@Ainurizams-MacBook-2.local>
2017-02-28 10:03:01 +0800

    Add first version of solution to oldestItemFromPlanet

--- Alien Adventure/OldestItemFromPlanet.swift
+++ Alien Adventure/OldestItemFromPlanet.swift
@@ -9,6 +9,34 @@
 extension Hero {
     
     func oldestItemFromPlanet(inventory: [UDItem], planet: String) -> UDItem? {
+        let planetOfOrigin = planet
+        var itemsForAlien = [UDItem]()
+        
+        // Use historicalData to find PlanetOfOrigin: Cuni and store in a new array
+        for item in inventory {
+            if let planetName = item.historicalData["PlanetOfOrigin"] as? String, planetName == "\(planetOfOrigin)" {
+                itemsForAlien.append(item)
+            }
+        }
+        
+        // Use historicalData to find the oldest CarbonAge from Array
+        // Return the oldest item to Alien
+        var itemMaxCarbonAge = 0
+        for item in itemsForAlien {
+            if let itemCarbonAge = item.historicalData["CarbonAge"] as? Int {
+                if itemCarbonAge >= itemMaxCarbonAge {
+                    itemMaxCarbonAge = itemCarbonAge
+                    itemsForAlien.remove(at: 0)
+                    print("The maximum age is \(itemMaxCarbonAge)") // Test age is iterating in loop
+                } else {
+                    return oldestItemForAlien
+                }
+            }
+        }
+        
+        print("Number of itemsForAlien in \(itemsForAlien.count)") // Test number of items
+        
+        
         return nil
     }
     
