8010ea3c0c2c42552b4e1b7a24be8de71b97ab1d
Ainurizam Ainuddin <rizainuddin@ainurizams-macbook-2.local>
2017-03-06 18:38:52 +0800

    Solve XOR cipher exercise

--- Alien Adventure/XORCipherKeySearch.swift
+++ Alien Adventure/XORCipherKeySearch.swift
@@ -15,25 +15,39 @@ extension Hero {
         // NOTE: This code doesn't exactly mimic what is in the Lesson. We've
         // added some print statements so that there are no warnings for 
         // unused variables 😀.
-        var key: UInt8
-        key = 0
         
-        for x in UInt8.min..<UInt8.max {
+        print("The encrypted message is \(encryptedString)") // yhmoexu
+        
+        var key: UInt8
+        key = 0 //If encryptedString is 0 return 0
+        
+        for x in UInt8.min..<UInt8.max { //Loop to check possible values of UInt8
             
             print(x)
             
             var decrypted: [UInt8]
-            decrypted = [UInt8]()
+            decrypted = [UInt8]() //Empty array to store each decrypted value
             
-            for character in encryptedString {
+            for character in encryptedString { //Loop each UInt8 encryptedString
                 // ADD CODE: perform decryption
-                print(character)
+                
+                print("Character in \(encryptedString) : \(character)")
+                
+                let performDecryption = character ^ x
+                decrypted.append(performDecryption)
+                
+                print("Characters decrypted: \(performDecryption)")
             }
+            
+            print("Decrypted values: \(decrypted)")
             
             if let decryptedString = String(bytes: decrypted,
                 encoding: String.Encoding.utf8), decryptedString == "udacity" {
                     // ADD CODE: found match, now what?
+                key = x
             }
+            
+            print("Key values: \(key)")
         }
         
         return key
