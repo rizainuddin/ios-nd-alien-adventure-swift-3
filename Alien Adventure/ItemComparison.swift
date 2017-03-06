3f41ae16d9232fb2c1dc8ffe2cdb46aba4111df0
Ainurizam Ainuddin <rizainuddin@ainurizams-macbook-2.local>
2017-03-06 23:06:14 +0800

    Solve Item Comparison exercise

--- Alien Adventure/ItemComparison.swift
+++ Alien Adventure/ItemComparison.swift
@@ -9,7 +9,11 @@
 import Foundation
 
 func <(lhs: UDItem, rhs: UDItem) -> Bool {
-    return true
+    
+    print("Left hand side: \(lhs)")
+    print("Right hand side: \(rhs)")
+    
+    return (lhs.rarity.rawValue <= rhs.rarity.rawValue) && (lhs.baseValue < rhs.baseValue)
 }
 
 // If you have completed this function and it is working correctly, feel free to skip this part of the adventure by opening the "Under the Hood" folder, and making the following change in Settings.swift: "static var RequestsToSkip = 5"
