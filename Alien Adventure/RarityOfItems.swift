4734b2be227d4515534b460623268db98547c35e
Ainurizam Ainuddin <rizainuddin@ainurizams-macbook-2.local>
2017-03-08 09:40:33 +0800

    Solve Rarity of Items using fwitch statements

--- Alien Adventure/RarityOfItems.swift
+++ Alien Adventure/RarityOfItems.swift
@@ -9,7 +9,41 @@
 extension Hero {
     
     func rarityOfItems(inventory: [UDItem]) -> [UDItemRarity:Int] {
-        return [UDItemRarity:Int]()
+        
+        //        print("Total items in inventory: \(inventory.count)")
+        
+        var rarityItemsForAlien: [UDItemRarity:Int]
+        rarityItemsForAlien = [UDItemRarity.common : 0, UDItemRarity.uncommon : 0, UDItemRarity.rare : 0, UDItemRarity.legendary : 0]
+        
+        var countCommon = 0
+        var countUncommon = 0
+        var countRare = 0
+        var countLegendary = 0
+        
+        //        print("Before loop: \(rarityItemsForAlien)")
+        
+        for item in inventory {
+            switch item.rarity {
+            case .common:
+                countCommon += 1
+                rarityItemsForAlien[UDItemRarity.common] = countCommon
+            case .uncommon:
+                countUncommon += 1
+                rarityItemsForAlien[UDItemRarity.uncommon] = countUncommon
+            case .rare:
+                countRare += 1
+                rarityItemsForAlien[UDItemRarity.rare] = countRare
+            case .legendary:
+                countLegendary += 1
+                rarityItemsForAlien[UDItemRarity.legendary] = countLegendary
+            default:
+                break
+            }
+        }
+        
+        //        print("After loop: \(rarityItemsForAlien)")
+        
+        return rarityItemsForAlien
     }
 }
 
